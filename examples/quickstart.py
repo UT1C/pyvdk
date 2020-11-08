ada8abc5a6e8d162f8b700913599bb9de27e64fb
lightmanLP <liteman1000@gmail.com>
2020-11-08 20:24:12 +0700

    example

---
+++ examples/quickstart.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from pyvdk import Bot, Config
+
+
+app = Flask(__name__)
+config = Config(
+    token="",
+    v="5.103",
+    group_id=,
+    secret="",
+    confcode=""
+)
+bot = Bot(config)
+
+
+@app.route('/', methods=['POST'])
+def bot_route():
+    return bot.request_handle(request.get_json())
+
+
+@bot.on.message_new(text='test')
+def test_func(msg: object):
+    print('brrrr')
+    msg('brrrrrrr')
